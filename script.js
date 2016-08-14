0a0c93a99d7d31eddb733b577779fa941349936b
An1on <i.love.m.farmer@gmail.com>
2016-08-14 13:39:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 'use strict';
 
-function Gallery() {
-    
+function Gallery(options) {
+    this.el = options.el;
+    this.mainImg = options.mainImg;
+    this._initEvent();
 }
+
+Gallery.prototype.handler = function(event) {
+    event.preventDefault();
+    if (event.target.hasAttributes('alt')) {
+        var attrName = event.target.getAttribute('alt');
+        this.mainImg.src = event.currentTarget.children[attrName].children[0].href;
+    }
+
+};
+
+Gallery.prototype._initEvent = function() {
+
+    this.el.addEventListener('click', this.handler.bind(this));
+
+};
+
+var gallery = new Gallery({
+    el: document.querySelector('.gallery_picture'),
+    mainImg: document.querySelector('[alt="Large image"]')
+});
