036ef28d21c3c0829e92005711c3fcc3b309cb67
vagabonde <anton.ts1994@gmail.com>
2020-06-25 16:24:37 +0300

    change test page

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.error('This is just the test');
+const [info, phases] = [].slice.apply(document.querySelector('.buttons').children);
+
+function sendInfoAboutSettings() {
+  console.error('send');
+
+}
+
+function sendInfoAboutSkips() {
+
+}
+
+function sendAboutUsers() {
+    setTimeout(() => {
+
+    }, 3600000)
+}
+
+window.addEventListener('click', sendInfoAboutSettings);
